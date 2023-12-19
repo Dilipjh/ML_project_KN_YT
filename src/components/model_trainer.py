8e67dec06471add1b31c9c5813f15a0baaa22bd9
Dilipjh <djhiremath@gmail.com>
2023-12-19 20:25:30 +0100

    model trainer with parameters

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -12,6 +12,7 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 from catboost import CatBoostRegressor
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomExceptionMessage
 from src.logger import logging
@@ -37,29 +38,102 @@ class ModelTrainer:
             )
 
             models_list = {
-                "RandomForestRegressor": RandomForestRegressor(),
-                "GradientBoostingRegressor": GradientBoostingRegressor(),
-                "AdaBoostRegressor": AdaBoostRegressor(),
+                "Random Forest Regressor": RandomForestRegressor(),
+                "Gradient Boosting Regressor": GradientBoostingRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor(),
                 "LogisticRegression": LogisticRegression(),
-                "LinearRegression": LinearRegression(),
+                "Linear Regression": LinearRegression(),
                 "Ridge": Ridge(),
                 "Lasso": Lasso(),
                 "KNeighborsRegressor": KNeighborsRegressor(),
-                "DecisionTreeRegressor": DecisionTreeRegressor(),
+                "Decision Tree Regressor": DecisionTreeRegressor(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoostRegressor": CatBoostRegressor(verbose=False),
+                "CatBoost Regressor": CatBoostRegressor(verbose=False),
+            }
+
+            params = {
+                "Random Forest Regressor": {
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2', None],
+                    "n_estimators": [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting Regressor": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "AdaBoost Regressor": {
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                    "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                    # 'loss':['linear','square','exponential'],
+                },
+                "LogisticRegression": {},
+                "Linear Regression": {},
+                "Ridge": {},
+                "Lasso": {},
+                "K neighbours Regressor": {
+                    "n_neighbors": [5, 7, 9, 11],
+                    # 'weights':['uniform','distance'],
+                    # 'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "Decision Tree Regressor": {
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "XGBRegressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "CatBoosting Regressor": {
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100],
+                },
             }
 
             report = {}
 
             for i in range(len(list(models_list))):
                 model_in_consideration = list(models_list.values())[i]
+                """ if list(models_list.keys())[i] in list(params.keys()):
+                    parameters_for_model = list(models_list.keys())[i]
+                    logging.info("parameter values found")
+                    print(
+                        "parameter values found for {}".format(
+                            list(models_list.keys())[i]
+                        )
+                    )
+                else:
+                    parameters_for_model = "no parameter"
+                    logging.info(
+                        "no parameter for {}".format(list(models_list.keys())[i])
+                    )
+                    print("no parameter for {}".format(list(models_list.keys()))[i]) """
+
+                parameters_for_model = params[list(params.keys())[i]]
+
+                gs = GridSearchCV(model_in_consideration, parameters_for_model, cv=3)
+                gs.fit(X_train_features, Y_train)
+
+                model_in_consideration.set_params(**gs.best_params_)
                 model_in_consideration.fit(X_train_features, Y_train)
+
                 Y_test_predicted = model_in_consideration.predict(X_test_features)
                 mae, mse, r2_value = model_evaliation(
                     Y_predicted=Y_test_predicted, Y_test=Y_test
                 )
                 report[list(models_list.keys())[i]] = r2_value
+                print("{0} model was run".format(model_in_consideration))
+                logging.info("{0} model was run".format(model_in_consideration))
 
             best_score = max(sorted(report.values()))
             best_model_fun_name = list(report.keys())[
